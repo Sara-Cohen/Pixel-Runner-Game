3f4474ae5e093dd5426a52ee832283dc4ea4325f
Sara <cohensara02@gmail.com>
2023-11-28 15:40:01 +0200

    add player and move it

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ text_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 text_surface = text_font.render('My game', False, 'Black')
 
 snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_rect = snail_surface.get_rect(midbottom=(800, 300))
+snail_rect = snail_surface.get_rect(bottomleft=(800, 300))
 
 player_surf = pygame.image.load('graphics/Player/player_walk_1.png')
 player_rect = player_surf.get_rect(midbottom=(80, 300))
@@ -35,8 +35,10 @@ while True:
         snail_rect.left = 800
     screen.blit(player_surf, player_rect)
     if player_rect.left < 800:
-        player_rect.right += 4
+        player_rect.right += 1
     else:
         player_rect.right = -100
+    if player_rect.colliderect(snail_rect):
+        print('oops!')
     pygame.display.update()
     clock.tick(60)
