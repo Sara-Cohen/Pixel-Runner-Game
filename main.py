a20b2f6975eb4b453fe5d039847405ea79b92586
Sara <cohensara02@gmail.com>
2023-11-28 13:44:10 +0200

    add sky and ground

--- main.py
+++ main.py
@@ -7,18 +7,17 @@ screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption("Runner")
 clock = pygame.time.Clock()
 
-test_surface = pygame.Surface((100, 200))
-test_surface.fill('Blue')
-
-test_surface2 = pygame.Surface((10, 20))
-test_surface2.fill('Green')
+sky_surface = pygame.image.load('graphics/Sky.png')
+ground_surface = pygame.image.load('graphics/ground.png')
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
-    screen.blit(test_surface, (200, 100))
-    screen.blit(test_surface2,(100,50))
+
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, 250))
+
 
     pygame.display.update()
     clock.tick(60)
