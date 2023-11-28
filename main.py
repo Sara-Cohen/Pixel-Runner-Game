3d91374e29f33f0f2ca9050dd1514ca6184e7133
Sara <cohensara02@gmail.com>
2023-11-28 12:42:21 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+from sys import exit
+
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((800, 400))
+pygame.display.set_caption("Runner")
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((100, 200))
+test_surface.fill('Blue')
+
+test_surface2 = pygame.Surface((10, 20))
+test_surface2.fill('Green')
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+    screen.blit(test_surface, (200, 100))
+    screen.blit(test_surface2,(100,50))
+
+    pygame.display.update()
+    clock.tick(60)
