f78eb0ce4fb090173dca3878a1d4f841ef1d1a9b
Sara <cohensara02@gmail.com>
2023-11-28 14:27:22 +0200

    Adding a hedgehog, and moving it on the screen

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ pygame.init()
 screen = pygame.display.set_mode((800, 400))
 pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
-text_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('graphics/Sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
+text_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 text_surface = text_font.render('My game', False, 'Black')
+snail_surface = pygame.image.load('graphics/snail/snail1.png')
+snail_x_pos = 800
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -20,6 +23,11 @@ while True:
     screen.blit(sky_surface, (0, 0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (300, 50))
+    screen.blit(snail_surface, (snail_x_pos, 265))
+    if snail_x_pos <= -100:
+        snail_x_pos = 800
+    else:
+        snail_x_pos -= 4
 
     pygame.display.update()
     clock.tick(60)
