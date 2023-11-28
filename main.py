a8bf2346debdaec4a97afb17fca986f345c430c5
Sara <cohensara02@gmail.com>
2023-11-28 14:04:56 +0200

    add text and design font

--- main.py
+++ main.py
@@ -4,11 +4,13 @@ import pygame
 
 pygame.init()
 screen = pygame.display.set_mode((800, 400))
-pygame.display.set_caption("Runner")
+pygame.display.set_caption('Runner')
 clock = pygame.time.Clock()
+text_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
 sky_surface = pygame.image.load('graphics/Sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
+text_surface = text_font.render('My game', False, 'Black')
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -16,8 +18,8 @@ while True:
             exit()
 
     screen.blit(sky_surface, (0, 0))
-    screen.blit(ground_surface, (0, 250))
-
+    screen.blit(ground_surface, (0, 300))
+    screen.blit(text_surface, (300, 50))
 
     pygame.display.update()
     clock.tick(60)
